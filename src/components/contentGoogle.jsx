f103e21d2c605bf12e4e4668f386c4784b36fd3e
Anoplay <agungprasetyo1111@gmail.com>
2019-01-30 16:28:33 +0700

    memperbaiki gambar

--- src/components/contentGoogle.jsx
+++ src/components/contentGoogle.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './app.css';
-import img from './googlelogo_color_272x92dp.png';
+// import img from './googlelogo_color_272x92dp.png';
 
 const ContentGoogle = props =>{
     return(
 
         <div className="content-google">
-            <img src={img} className="img" />
+            <img src={require('./googlelogo_color_272x92dp.png')} className="img" />
         </div>
             
     )
